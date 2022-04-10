049e549fbb0bace4534f0b42f891ffa62b25c687
dazh5587 <dazh5587@colorado.edu>
2022-04-09 18:40:12 -0600

    user log in/log out works and signup too

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
-  get 'signup'=>'users#new'
   get 'help'=>'static_pages#help'
-  get  'about'=>'static_pages#about'
+  get 'about'=>'static_pages#about'
+  get 'signup'=>'users#new'
+  get 'login'=>'sessions#new'
+  post 'login'=>'sessions#create'
+  get 'logout'=>'sessions#destroy'
   resources :users
 end
